33c2b0ca25c3c711daab50ecf262e0a217ade6ce
Jairo Mancebo <jairolmr@hotmail.com>
2023-07-20 00:08:32 -0400

    Add initial tests for Feria Del Libro

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,11 +3,10 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   video: false,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config)
     },
-    baseUrl: 'https://demoqa.com/',
+    baseUrl: 'https://fil2023-73crka.netlify.app',
+    testIsolation: false,
   },
 })
--- cypress/e2e/tests/example_tests.cy.ts
+++ cypress/e2e/tests/example_tests.cy.ts
@@ -3,31 +3,35 @@ describe('Example Functional Tests', () => {
     cy.visit('/');
   });
 
-  it('should navigate to the homepage of qa demo', () => {
-    cy.get('header > a > img').should('be.visible');
-    cy.url().should('be.equal', 'https://demoqa.com/');
+  it('should navigate to the homepage', () => {
+    cy.contains('Programa').should('be.visible');
+    cy.url().should('be.equal', 'https://fil2023-73crka.netlify.app/');
   });
 
-  it('should navigate to the elements card', () => {
-    cy.get('.category-cards > :nth-child(1)').should('be.visible').click();
-    cy.get('.main-header').should('be.visible').should('contain', 'Elements');
+  it('should open the event details of an event', () => {
+    // cy.visit('/');
+    cy.get('tr > td > div > :nth-child(1)').first().click();
+    cy.get('h4').contains('Evento').should('be.visible');
+    cy.contains('Programa').click({ force: true, multiple: true });
+    cy.get('body').click(0,0);
+    cy.get('h4').contains('Evento').should('not.be.visible');
   });
 
-  it('should fill out the text box form', () => {
-    cy.get(':nth-child(1) > .group-header > .header-wrapper').should('be.visible');
-    cy.get(':nth-child(1) > .element-list > .menu-list > #item-0')
-      .should('be.visible')
-      .should('contain', 'Text Box')
-      .click();
-    cy.get('#userName').type('Test');
-    cy.get('#userEmail').type('Test@Test.com');
-    cy.get('#currentAddress').type('Test');
-    cy.get('#permanentAddress').type('Test');
-    cy.get('#submit').click();
-
-    cy.get('#name').should('contain', 'Test');
-    cy.get('#email').should('contain', 'Test@Test.com');
-    cy.get('.border > #currentAddress').should('contain', 'Test');
-    cy.get('.border > #permanentAddress').should('contain', 'Test');
-  });
+  // it('should fill out the text box form', () => {
+  //   cy.get(':nth-child(1) > .group-header > .header-wrapper').should('be.visible');
+  //   cy.get(':nth-child(1) > .element-list > .menu-list > #item-0')
+  //     .should('be.visible')
+  //     .should('contain', 'Text Box')
+  //     .click();
+  //   cy.get('#userName').type('Test');
+  //   cy.get('#userEmail').type('Test@Test.com');
+  //   cy.get('#currentAddress').type('Test');
+  //   cy.get('#permanentAddress').type('Test');
+  //   cy.get('#submit').click();
+  //
+  //   cy.get('#name').should('contain', 'Test');
+  //   cy.get('#email').should('contain', 'Test@Test.com');
+  //   cy.get('.border > #currentAddress').should('contain', 'Test');
+  //   cy.get('.border > #permanentAddress').should('contain', 'Test');
+  // });
 });
